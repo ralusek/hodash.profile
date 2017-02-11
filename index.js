e0f96978a989ffbaf0e9c577f6f5fcf5f04c3d58
ralusek <ralusek@gmail.com>
2017-02-10 22:10:58 -0800

    Changed to use a timer class.

--- index.js
+++ index.js
@@ -4,8 +4,53 @@
 /**
  *
  */
-module.exports = function() {
-  const timer = {started: Date.now()};
-  timer.time = () => Date.now() - timer.started;
-  return Object.freeze(timer);
+module.exports = function(name) {
+  return new Timer(name);
 };
+
+
+
+// This establishes a private namespace.
+const namespace = new WeakMap();
+function p(object) {
+  if ( namespace.has(object)) namespace.set(object, {});
+  return namespace.get(object);
+}
+
+
+
+/**
+ *
+ */
+class Timer {
+  constructor(name) {
+    p(this).name = name;
+
+    p(this).start = Date.now()
+  }
+
+  /**
+   *
+   */
+  end() {
+    if(!p(this).end) {
+      (p(this).end = Date.now());
+      p(this).timeTaken = p(this).end - p(this).start;
+    }
+  }
+
+  /**
+   *
+   */
+  output() {
+    const output = {
+      start: p(this).start,
+      end: p(this).end,
+      timeTaken: p(this).timeTaken
+    };
+
+    if (p(this).name) output.name = p(this).name;
+
+    return output;
+  }
+}
